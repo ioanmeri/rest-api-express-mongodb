95f8b7e5c251db9e526b505ae036f62ee7dde83a
ioanmeri <ioannismeri@gmail.com>
2019-12-24 09:56:16 +0200

    add subscriber model

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 // Getting all
-router.get('/', (req, res) => {
-  res.send('Hellow World')
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch(err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 // Getting one
 router.get('/:id', (req, res) => {
