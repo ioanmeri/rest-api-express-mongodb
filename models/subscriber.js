38233956010b70a9d5ba672c74ead185a1477d9a
ioanmeri <ioannismeri@gmail.com>
2019-12-24 10:31:14 +0200

    add crud routes

--- models/subscriber.js
+++ models/subscriber.js
@@ -5,7 +5,7 @@ const subscriberSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  subsciberToChannel: {
+  subscriberToChannel: {
     type: String,
     required: true
   },
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -12,21 +12,62 @@ router.get('/', async (req, res) => {
   }
 })
 // Getting one
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber)
 })
 // Creating one
-router.post('/', (req, res) => {
-  req.params.id
+router.post('/', async (req, res) => {
+  console.log(req.body)
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscriberToChannel: req.body.subscriberToChannel
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
 // Updating one
-router.patch('/:id', (req, res) => {
-  
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if(req.body.name != null){
+    res.subscriber.name = req.body.name
+  }
+  if(req.body.subscriberToChannel != null){
+    res.subscriber.subscriberToChannel = req.body.subscriberToChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
 // Deleting one
-router.delete('/:id', (req, res) => {
-  
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted Subscriber' })
+  } catch(err) {
+    res.status(500).json({ message: err.message })
+  }
 })
+
+async function getSubscriber(req, res, next){
+  try {
+    subscriber = await Subscriber.findById(req.params.id)
+    if(subscriber == null){
+      return res.status(404).json({ message: 'Cannot find subscriber' })
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+
+  res.subscriber = subscriber
+  next()
+}
+
 module.exports = router
